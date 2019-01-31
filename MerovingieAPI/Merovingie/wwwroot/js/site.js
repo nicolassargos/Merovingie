ba7dbeaeef95d9ccdaeecf582cfa800dc7ea2ac4
nsargos <lamenagerefolle@hotmail.com>
2019-01-31 07:33:04 -0800

    Ajout d'un data service pour Websockets - part 1

--- MerovingieAPI/Merovingie/wwwroot/js/site.js
+++ MerovingieAPI/Merovingie/wwwroot/js/site.js
@@ -1,4 +1,7 @@
 ﻿import { MMessage } from './MMessage.js';
+import { DataService } from './data-service.js';
+
+
 
 
 var connection = document.getElementById("connectionForm");
@@ -13,138 +16,140 @@ var commsLog = document.getElementById("commsLog");
 
 var loadbutton = document.getElementById("loadbutton");
 
-var socket;
-var scheme = document.location.protocol === "https:" ? "wss" : "ws";
-var port = document.location.port ? (":" + document.location.port) : "";
-
 // Message qui sera envoyé au serveur-
 var mMessage = new MMessage();
 
-// Variable de connexion qui contient l'adresse du serveur
-connectionUrl.value = scheme + "://" + document.location.hostname + port + "/ws";
+// Initialise le service de Websocket
+var dataService = new DataService();
+
 
 // Mets à jour l'interface selon l'état du Websocket
-function updateState() {
-    function disable() {
-        sendMessage.disabled = true;
-        sendButton.disabled = true;
-        closeButton.disabled = true;
-    }
-    function enable() {
-        sendMessage.disabled = false;
-        sendButton.disabled = false;
-        closeButton.disabled = false;
-    }
+//function updateState() {
+//    function disable() {
+//        sendMessage.disabled = true;
+//        sendButton.disabled = true;
+//        closeButton.disabled = true;
+//    }
+//    function enable() {
+//        sendMessage.disabled = false;
+//        sendButton.disabled = false;
+//        closeButton.disabled = false;
+//    }
 
-    connectionUrl.disabled = true;
-    connectButton.disabled = true;
+//    connectionUrl.disabled = true;
+//    connectButton.disabled = true;
 
-    if (!socket) { disable(); }
-    else {
-        switch (socket.readyState) {
-            case WebSocket.CLOSED:
-                stateLabel.innerHTML = "Closed";
-                disable();
-                connectionUrl.disabled = false;
-                connectButton.disabled = false;
-                break;
-            case WebSocket.CLOSING:
-                stateLabel.innerHTML = "Closing...";
-                disable();
-                break;
-            case WebSocket.CONNECTING:
-                stateLabel.innerHTML = "Connecting...";
-                disable();
-                break;
-            case WebSocket.OPEN:
-                stateLabel.innerHTML = "Open";
-                enable();
-                break;
-            default:
-                stateLabel.innerHTML = "Unknown state...:" + htmlEscape(socket.readyState);
-                disable();
-                break;
-        }
-    }
-}
+//    if (!socket) { disable(); }
+//    else {
+//        switch (socket.readyState) {
+//            case WebSocket.CLOSED:
+//                stateLabel.innerHTML = "Closed";
+//                disable();
+//                connectionUrl.disabled = false;
+//                connectButton.disabled = false;
+//                break;
+//            case WebSocket.CLOSING:
+//                stateLabel.innerHTML = "Closing...";
+//                disable();
+//                break;
+//            case WebSocket.CONNECTING:
+//                stateLabel.innerHTML = "Connecting...";
+//                disable();
+//                break;
+//            case WebSocket.OPEN:
+//                stateLabel.innerHTML = "Open";
+//                enable();
+//                break;
+//            default:
+//                stateLabel.innerHTML = "Unknown state...:" + htmlEscape(socket.readyState);
+//                disable();
+//                break;
+//        }
+//    }
+//}
 
-//
-//
-//
-closeButton.onclick = function () {
-    if (!socket || socket.readyState !== WebSocket.OPEN) {
-        alert("socket not connected");
-    }
-    socket.close(1000, "closing from client");
-}
+////
+////
+////
+//closeButton.onclick = function () {
+//    if (!socket || socket.readyState !== WebSocket.OPEN) {
+//        alert("socket not connected");
+//    }
+//    socket.close(1000, "closing from client");
+//}
 
-//
-//
-//
-sendButton.onclick = function () {
-    if (!socket || socket.readyState !== WebSocket.OPEN) {
-        alert("socket not connected");
-    }
-    if (sendMessage.value && sendMessage.value.length > 0) {
-        mMessage.type = 'worker';
-        mMessage.message = sendMessage.value;
+////
+////
+////
+//sendButton.onclick = function () {
+//    if (!socket || socket.readyState !== WebSocket.OPEN) {
+//        alert("socket not connected");
+//    }
+//    if (sendMessage.value && sendMessage.value.length > 0) {
+//        mMessage.type = 'worker';
+//        mMessage.message = sendMessage.value;
 
-        var jsonData = JSON.stringify(mMessage)
+//        var jsonData = JSON.stringify(mMessage)
 
-        socket.send(jsonData);
+//        socket.send(jsonData);
 
-        commsLog.innerHTML += '<tr>' +
-            '<td>Client</td>' +
-            '<td>Server</td>' +
-            '<td>' + htmlEscape(mMessage.message) + '</td>'
-            + '</tr>';
-    }
-}
+//        commsLog.innerHTML += '<tr>' +
+//            '<td>Client</td>' +
+//            '<td>Server</td>' +
+//            '<td>' + htmlEscape(mMessage.message) + '</td>'
+//            + '</tr>';
+//    }
+//}
 
-//
-//
-//
-connectButton.onclick = function () {
-    stateLabel.innerHTML = "connection.....";
-    socket = new WebSocket(connectionUrl.value);
+////
+////
+////
+//connectButton.onclick = function () {
+//    stateLabel.innerHTML = "connection.....";
+    
 
-    // 
-    socket.onopen = function (e) {
-        updateState();
-        commsLog.innerHTML += '<tr><td colspan="3"> Connection opened </td></tr>';
-    };
+//    // 
+//    socket.onopen = function (e) {
+//        updateState();
+//        commsLog.innerHTML += '<tr><td colspan="3"> Connection opened </td></tr>';
+//    };
 
-    // 
-    socket.onclose = function (event) {
-        updateState();
-        commsLog.innerHTML += '<tr><td colspan="3"> Connection closed. Code:' +
-            htmlEscape(event.code) + '. Reason: ' + htmlEscape(event.reason) + '. </td ></tr > ';
-    };
+//    // 
+//    socket.onclose = function (event) {
+//        updateState();
+//        commsLog.innerHTML += '<tr><td colspan="3"> Connection closed. Code:' +
+//            htmlEscape(event.code) + '. Reason: ' + htmlEscape(event.reason) + '. </td ></tr > ';
+//    };
 
-    //
-    socket.onerror = updateState();
+//    //
+//    socket.onerror = updateState();
 
-    //
-    socket.onmessage = function (event) {
+//    //
+//    socket.onmessage = function (event) {
 
-        var messageReceived = JSON.parse(event.data);
+//        var messageReceived = JSON.parse(event.data);
 
-        commsLog.innerHTML += '<tr>' +
-            '<td>Server</td>' +
-            '<td>Client</td>' +
-            '<td>' + htmlEscape(messageReceived.Message) + '</td>'
-            + '</tr>';
-        console.log(messageReceived.Message);
-    };
+//        commsLog.innerHTML += '<tr>' +
+//            '<td>Server</td>' +
+//            '<td>Client</td>' +
+//            '<td>' + htmlEscape(messageReceived.Message) + '</td>'
+//            + '</tr>';
+//        console.log(messageReceived.Message);
+//    };
 
 
-}
+//}
 
-function htmlEscape(str) {
-    return str
-        .replace(/&/g, '&amp;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#39;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-}
+//function htmlEscape(str) {
+//    return str
+//        .replace(/&/g, '&amp;')
+//        .replace(/"/g, '&quot;')
+//        .replace(/'/g, '&#39;')
+//        .replace(/</g, '&lt;')
+//        .replace(/>/g, '&gt;');
+//}
+
+
+////////
+connectionUrl.value = dataService.connectionUrl;
+alert('Websocket connecté');
